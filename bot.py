716f5db2b07bf4b5583b5afe1d0170771b8435a5
Stanislav Pupkov <stanislav.pupkov@eltex.loc>
2020-05-30 17:56:44 +0700

    1

---
+++ bot.py
@@ -0,0 +1,27 @@
+import telebot
+import config
+
+bot = telebot.TeleBot(config.token)
+
+keyboard1 = telebot.types.ReplyKeyboardMarkup()
+keyboard1 = telebot.types.ReplyKeyboardMarkup(True)
+keyboard1.row('Привет', 'Пока')
+keyboard1.row('/start')
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard1)
+
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, 'Привет, мой создатель')
+    if message.text.lower() == 'пока':
+        bot.send_message(message.chat.id, 'Прощай, создатель')
+    if message.text == 'Love':
+        bot.send_sticker(message.chat.id, 'CAADAgADUAkAAnlc4gn1AAE2sQABMfNIAg')
+
+
+bot.polling()
---
+++ config.py
@@ -0,0 +1,2 @@
+token = '875689477:AAH49j48TBWQkEb2Ah0gfKA56hN7A0Sk4VM'
+app_name = 'MiniVanBot'
