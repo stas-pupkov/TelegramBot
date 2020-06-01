3217495ee381662821710a07eee55c9e78fea135
Stanislav Pupkov <stanislav.pupkov@eltex.loc>
2020-06-01 16:08:53 +0700

    24

--- tickets/bot.py
+++ tickets/bot.py
@@ -14,57 +14,56 @@ from telebot import types
 bot = telebot.TeleBot(config.token)
 
 keyboard1 = types.ReplyKeyboardMarkup(True)
-keyboard1.row('Привет', 'Пока', '1')
+keyboard1.row('Привет', 'ABA', 'OVB')
 keyboard1.row('/start')
 
 
+def get_fun(from_city):
+    response = get_countries(from_city)
+    if 'Unknown city iata' in str(response):
+        return 'Error'
+    else:
+        output = ''
+        for i in response:
+            price = i['value']
 
-
-# headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36',
-#            'X-Access-Token': 'f14fe0ba8027435931a2f6aac4281692',
-#            'Accept-Encoding': 'gzip, deflate'}
-
-
-
-def get_fun():
-    url = "http://api.travelpayouts.com/v1/prices/cheap"
-    querystring = {"origin": "MOW", "destination": "HKT"}
-    headers = {'X-Access-Token': 'f14fe0ba8027435931a2f6aac4281692',
-               'Accept-Encoding': 'gzip'}
-
-    ur = 'http://map.aviasales.ru/prices.json?origin_iata=ABA&period=2020-06-01:season&direct=true&one_way=false&no_visa=true&schengen=true&need_visa=true&locale=ru&min_trip_duration_in_days=1&max_trip_duration_in_days=2'
-
-    response = requests.get(ur).json()
-    # response = requests.get(url, headers=headers, params=querystring).json()
-    #
-    # json_str = json.dumps(response)
-    # resp = json.loads(json_str)['data']['HKT']
-    #
-    # for (k, v) in resp.items():
-    #     print(k + ' ' + str(v))
-
-    print(response)
-
-    output = ''
-
-    for i in response:
-        url = 'https://www.air-port-codes.com/search/results/' + i['destination']
-
-        full_page = requests.get(url)
-        soup = BeautifulSoup(full_page.content, 'html.parser')
-        table = soup.findAll('table', 'small')[0]
-
-        name = table.select('a')[0].text
-        country = table.select('a')[3].text
-
-        output += ('- ' + name + ', ' + country + '\n')
-
+            if price < 40000:
+                destination_iata = i['destination']
+                airport, country = get_info_iata(destination_iata)
+                if country in config.europe:
+                    output += ('- ' + airport + ', ' + country + '\nPrice: ' + str(price) + ' rub\n\n')
     return output
 
 
-def get_countries(iata):
-    ur = 'http://map.aviasales.ru/prices.json?origin_iata='+iata+'&period=2020-06-01:season&direct=true&one_way=false&no_visa=true&schengen=true&need_visa=true&locale=ru&min_trip_duration_in_days=1&max_trip_duration_in_days=2'
-    response = requests.get(ur).json()
+def get_countries(from_city):
+    url = 'http://map.aviasales.ru/prices.json'
+    querystring = {'origin_iata': from_city,
+                   'period': '2020-06-01:month',
+                   'direct': True,
+                   'one_way': True,
+                   'no_visa': True,
+                   'schengen': True,
+                   'need_visa ': True,
+                   'locale': 'ru',
+                   'min_trip_duration_in_days ': 1,
+                   'max_trip_duration_in_days ': 2}
+    response = requests.get(url, params=querystring).json()
+    for i in response:
+        print(i)
+    return response
+
+
+def get_info_iata(iata):
+    url = 'https://www.air-port-codes.com/search/results/' + iata
+    full_page = requests.get(url)
+    soup = BeautifulSoup(full_page.content, 'html.parser')
+    table = soup.findAll('table', 'small')[0]
+
+    airport = table.select('a')[0].text
+    country = table.select('a')[3].text
+
+    return airport, country
+
 
 
 @bot.message_handler(commands=['start'])
@@ -75,8 +74,11 @@ def start_message(message):
 
 @bot.message_handler(content_types=["text"])
 def default_test(message):
-    if message.text.lower() == 'привет':
-        bot.send_message(message.chat.id, get_fun())
-
+    bot.send_message(message.chat.id, "Wait a second...")
+    msg = get_fun(message.text)
+    if len(msg) > 0:
+        bot.send_message(message.chat.id, msg)
+    else:
+        bot.send_message(message.chat.id, "Empty")
 
 bot.polling(none_stop=True)
