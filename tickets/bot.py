28418f9227191f1a0bb2312be1ad0b2495ae1544
Stanislav Pupkov <stanislav.pupkov@eltex.loc>
2020-06-01 14:44:18 +0700

    Получение списка стран

--- tickets/bot.py
+++ tickets/bot.py
@@ -28,13 +28,11 @@ keyboard1.row('/start')
 
 def get_fun():
     url = "http://api.travelpayouts.com/v1/prices/cheap"
-
     querystring = {"origin": "MOW", "destination": "HKT"}
-
     headers = {'X-Access-Token': 'f14fe0ba8027435931a2f6aac4281692',
                'Accept-Encoding': 'gzip'}
 
-    ur = 'http://map.aviasales.ru/prices.json?origin_iata=OVB&period=2020-06-01:season&direct=true&one_way=false&no_visa=true&schengen=true&need_visa=true&locale=ru&min_trip_duration_in_days=13&max_trip_duration_in_days=15'
+    ur = 'http://map.aviasales.ru/prices.json?origin_iata=ABA&period=2020-06-01:season&direct=true&one_way=false&no_visa=true&schengen=true&need_visa=true&locale=ru&min_trip_duration_in_days=1&max_trip_duration_in_days=2'
 
     response = requests.get(ur).json()
     # response = requests.get(url, headers=headers, params=querystring).json()
@@ -45,11 +43,28 @@ def get_fun():
     # for (k, v) in resp.items():
     #     print(k + ' ' + str(v))
 
+    print(response)
+
+    output = ''
+
     for i in response:
-        print(i)
+        url = 'https://www.air-port-codes.com/search/results/' + i['destination']
 
-    return str(response['data'])
+        full_page = requests.get(url)
+        soup = BeautifulSoup(full_page.content, 'html.parser')
+        table = soup.findAll('table', 'small')[0]
 
+        name = table.select('a')[0].text
+        country = table.select('a')[3].text
+
+        output += ('- ' + name + ', ' + country + '\n')
+
+    return output
+
+
+def get_countries(iata):
+    ur = 'http://map.aviasales.ru/prices.json?origin_iata='+iata+'&period=2020-06-01:season&direct=true&one_way=false&no_visa=true&schengen=true&need_visa=true&locale=ru&min_trip_duration_in_days=1&max_trip_duration_in_days=2'
+    response = requests.get(ur).json()
 
 
 @bot.message_handler(commands=['start'])
